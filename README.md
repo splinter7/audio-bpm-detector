825587bb3a6f5bea46e33a859948f5b15b38a535
Splinter7 <stewart@splinter7.com>
2018-08-05 21:32:40 -0400

    installed eslint and prettier

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 This is simple beat detection application. This app takes as input an mp3 file and outputs the BPM of the file. To use this app run the commands below.
 
-* `npm run-script transpile`
-* `npm start`
+- `npm install`
+- `npm run-script transpile`
+- `npm start`
--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
 import * as fs from "fs";
-import {AudioContext} from "web-audio-api";
+import { AudioContext } from "web-audio-api";
 import MusicTempo from "music-tempo";
 
-var calcTempo = function (buffer) {
-    var audioData = [];
-    // Take the average of the two channels
-    if (buffer.numberOfChannels == 2) {
-      var channel1Data = buffer.getChannelData(0);
-      var channel2Data = buffer.getChannelData(1);
-      var length = channel1Data.length;
-      for (var i = 0; i < length; i++) {
-        audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
-      }
-    } else {
-      audioData = buffer.getChannelData(0);
+const calcTempo = buffer => {
+  let audioData = [];
+  // Take the average of the two channels
+  if (buffer.numberOfChannels === 2) {
+    const channel1Data = buffer.getChannelData(0);
+    const channel2Data = buffer.getChannelData(1);
+    const { length } = channel1Data;
+    for (let i = 0; i < length; i += 1) {
+      audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
     }
-    var mt = new MusicTempo(audioData);
-   
-    console.log(mt.tempo);
-    //console.log(mt.beats);
+  } else {
+    audioData = buffer.getChannelData(0);
   }
-   
-  var data = fs.readFileSync("real_bongo_nyah.mp3");
-   
-  var context = new AudioContext();
-  context.decodeAudioData(data, calcTempo);
+  const mt = new MusicTempo(audioData);
+
+  console.log(mt.tempo);
+  // console.log(mt.beats);
+};
+
+const data = fs.readFileSync("real_bongo_nyah.mp3");
+
+const context = new AudioContext();
+context.decodeAudioData(data, calcTempo);
